bd665eeafbc67531288c017d820fa8c9d44baa06
Sanjana S <sanjana.gurukar@gmail.com>
2019-12-16 22:46:43 +0530

    Updates

---
+++ precache-manifest.f1617c5d8749b57121bc7239c39b8419.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d49f9c88e917569e3d42b6ec5229bb44",
+    "url": "/2048-Game/index.html"
+  },
+  {
+    "revision": "ee41b1ee208a566ad782",
+    "url": "/2048-Game/static/css/main.0c8be4f6.chunk.css"
+  },
+  {
+    "revision": "12ed7e1fae371b4cb936",
+    "url": "/2048-Game/static/js/2.e494a0c8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/2048-Game/static/js/2.e494a0c8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ee41b1ee208a566ad782",
+    "url": "/2048-Game/static/js/main.f85a0369.chunk.js"
+  },
+  {
+    "revision": "f7138c956ae8c1eae010",
+    "url": "/2048-Game/static/js/runtime-main.79b60a2d.js"
+  }
+]);
